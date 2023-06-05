2583a402cdc42aea07e3d42e01f54a51b7cfb25e
xb <ssbandjl@gmail.com>
2023-06-05 22:58:22 +0800

    spdk

--- config.sh
+++ config.sh
@@ -2,4 +2,5 @@
 # ./configure --with-shared
 
 # clear;./configure --with-daos --with-rdma --enable-werror --disable-unit-tests
-clear;./configure --with-daos --with-rdma --disable-unit-tests --enable-debug
+# clear;./configure --with-daos --with-rdma --disable-unit-tests --enable-debug
+clear;./configure --with-rdma --disable-unit-tests --enable-debug
