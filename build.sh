7d78a25d152d07dd244bff4093f34ea22cc1b009
xiaobing.song <xiaobingsong@yunify.com>
2023-07-16 00:00:05 +0800

    readme

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@
 
 # 配置和编译
 ./config.sh && make -j32
+
+#clear;./configure --with-daos --disable-tests --disable-unit-tests --disable-apps --without-vhost --without-crypto --without-rbd --with-rdma --without-iscsi-initiator --without-vtune --with-shared
+# ./configure --with-shared
+
+# clear;./configure --with-daos --with-rdma --enable-werror --disable-unit-tests
+# clear;./configure --with-daos --with-rdma --disable-unit-tests --enable-debug
+# clear;./configure --with-rdma --disable-unit-tests --enable-debug
